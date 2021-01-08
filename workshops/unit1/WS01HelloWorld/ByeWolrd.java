cb1c9a367fbeac775fbedf7e87c5c098ba3185d9
JustinAlex26 <justin-alex@live.com>
2021-01-08 13:35:51 -0500

    ByeWolrd.java

---
+++ workshops/unit1/WS01HelloWorld/ByeWolrd.java
@@ -0,0 +1,10 @@
+//  Justin Vasconez Bye World
+
+/*
+some lines of comments
+can be written in this way thanks
+*/
+class ByeWorld{
+           public static void main(String[] args{
+		   System.out.println("Bye World from Justin Vasconez"):
+		   }
