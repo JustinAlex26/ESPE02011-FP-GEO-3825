6fbf16add2aba517003eeb5e0d9be52809d8021e
JustinAlex26 <justin-alex@live.com>
2021-01-11 15:39:59 -0500

    WS04-LogicalOperations

--- FP02_control Statement/src/fp02_control/statement/FP02_controlStatement.java
+++ FP02_control Statement/src/fp02_control/statement/FP02_controlStatement.java
@@ -5,7 +5,7 @@
  */
 package fp02_control.statement;
 
-import java.util.Scanner;
+
 
 /**
  *
@@ -18,15 +18,5 @@ public class FP02_controlStatement {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    Scanner scanner = new Scanner(System.in); 
-    System.out.println("Please enter an integer--");
+   System.out.println("My ");
     
-        int num;
-    num = scanner.nextInt();
-    if (num % 2==0){
-        System.out.println("The number entered is even-- " + num);
-    } else {
-        System.out.println("The number entered is odd--" + num);
-        }
-    }
-}
