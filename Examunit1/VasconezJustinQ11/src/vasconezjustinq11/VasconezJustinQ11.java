5aaecb6ae811246e82352b51e5e9844ac8ec01de
JustinAlex26 <justin-alex@live.com>
2021-01-18 13:57:03 -0500

    VasconezJustinQ11

---
+++ Examunit1/VasconezJustinQ11/src/vasconezjustinq11/VasconezJustinQ11.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vasconezjustinq11;
+
+/**
+ *
+ * @author Usuario
+ */
+public class VasconezJustinQ11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ int i;
+ i=0;
+  System.out.println("Multipication Tables :7");
+                int top=12;
+                int product=0;
+                int table =6;
+                for (int j =6 ;j<=top ; j++){
+                    product = table*j;
+                    System.out.println("6 *" + j + "=" + product);
+                    
+                }
+    
+    }
+    
+}
--- assignments/unit1/HW04Menus/src/hw04menus/NewClass.java
+++ assignments/unit1/HW04Menus/src/hw04menus/NewClass.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package wh03;
+package hw04menus;
 
 /**
  *
  * @author Usuario
  */
-public class WH03 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-
-    }
+public class NewClass {
+    
 }
