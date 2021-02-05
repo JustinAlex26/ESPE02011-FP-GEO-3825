ff31cd0f8297b740466cd2e3b5d856c5007a14e3
JustinAlex26 <justin-alex@live.com>
2021-02-04 23:52:09 -0500

    WS10_Functions

---
+++ assignments/unit2/WS10_Functions/src/ws10_functions/WS10_Functions.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws10_functions;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class WS10_Functions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input= new Scanner(System.in);
+        int divd = 0;
+        int divr = 0;
+        float quotient = 0;
+        int x = 0;
+        int y = 0;
+        int multiplying =0;
+        int multiplier = 0;
+        int product = 0;
+        int table=0; 
+        quotient = divideTwoNumbers(divd, divr);
+
+        System.out.println("Chose the numer fot the dividend ");
+        divd = input.nextInt();
+        System.out.println("this is the number fot the divr");
+        divr = input.nextInt();
+        quotient= divideTwoNumbers(divd, divr);
+        System.out.println("Quotient of 5 and 10 is -> " + quotient); 
+        System.out.println("add the number");
+        x= input.nextInt();
+        y = f(x);
+        System.out.println(+ x + "^2 -> " + y);
+        System.out.println("add the number");
+        x = input.nextInt();
+        y = f(x);
+        System.out.println( + x +"^2 -> " + y); 
+        System.out.println("add the number");
+        x= input.nextInt();
+        y = f(x);
+        System.out.println("the square of -> " + x + " <- is equal to -> " + y);
+        x= input.nextInt();
+        y=f(x);
+        System.out.println("y = " + x + "*" + x + "+2*" + x + "+ 1 =" + y);
+        System.out.println(" put your number please");
+        multiplying= input.nextInt();
+        System.out.println(" add the number of the table -->");
+        table = input.nextInt();
+        for (int j = 1; j <= multiplying ; j ++ ){
+       product= table* j;
+            System.out.println(+ table +" * " + j + " = " + product);
+            
+           
+        }
+       System.out.println("<<<Justin Vasconez>>>");
+            System.out.println("===============Hasta la proxima=============="); 
+    }
+
+    public static float divideTwoNumbers(int dividend, int divisor) {
+  
+        float quotient = 0.0F;
+
+       
+        quotient = (float) dividend / (float) divisor;
+
+        return quotient;
+    }
+
+    public static int f(int x) {
+        int y = 0;
+
+        y = x * x;
+
+        return y;
+    }
+
+    public static int g(int x) {
+        int y = 0;
+
+        y = x * x + 2 * x + 1;
+
+        return y;
+    }
+public static int multiplicationtable( int multiplyig ,  int multiplier) {   
+    int multiplying =0;
+    int product=0;
+    int table=0;
+    
+    for (int j = 1 ; j <= multiplying ; j ++){
+    product = table * j;
+    System.out.println( + table + " * " + j + " = " + product);
+      
+     } 
+         return +product;
+}
+}
